e4a2fc1e3d0ac22e7fc7df8670371474202be2e3
Adam Fuller <fullerac@student.swosu.edu>
2022-04-26 19:31:01 -0500

    Update CovidProject Scripts.sql

--- CovidProject Scripts.sql
+++ CovidProject Scripts.sql
@@ -124,3 +124,55 @@ JOIN CovidProject..CovidVaccinations cv
 	on cd.location = cv.location
 	and cd.date = cv.date
 where cd.continent is not null
+
+
+
+
+___________________________________________________________________________________________________________________________________________________
+
+
+
+
+
+-- Queries used for Tableau Project
+
+
+
+
+-- 1. Global Numbers
+
+Select SUM(new_cases) as total_cases, SUM(cast(new_deaths as int)) as total_deaths, SUM(cast(new_deaths as int))/SUM(New_Cases)*100 as DeathPercentage
+From CovidProject..CovidDeaths
+where continent is not null 
+--Group By date
+order by 1,2
+
+
+
+
+-- 2. Total Deaths Per Continent
+
+
+Select location, SUM(cast(new_deaths as int)) as TotalDeathCount
+From CovidProject..CovidDeaths
+Where continent is null 
+and location not in ('World', 'European Union', 'International')
+Group by location
+order by TotalDeathCount desc
+
+
+-- 3. Percent Population Infected by Country
+
+Select Location, Population, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From CovidProject..CovidDeaths
+Group by Location, Population
+order by PercentPopulationInfected desc
+
+
+-- 4. Percent Population Infected 
+
+
+Select Location, Population,date, MAX(total_cases) as HighestInfectionCount,  Max((total_cases/population))*100 as PercentPopulationInfected
+From CovidProject..CovidDeaths
+Group by Location, Population, date
+order by PercentPopulationInfected desc
